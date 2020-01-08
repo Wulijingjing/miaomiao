2064a8dbae36fcd769e34fce5cc318e1888a6051
Wulijingjing <1131069347@qq.com>
2020-01-08 22:00:44 +0800

    createComponents

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="main">
+        <Header title="登录喵喵"></Header>
+        <div id="content">
+            <Login />
+        </div>
+        <TabBar></TabBar>
+    </div>
+</template>
+
+<script>
+    import Header from '@/components/Header';
+    import TabBar from '@/components/TabBar';
+    import Login from "../../components/Login/index";
+    export default {
+        name: "Mine",
+        components: {
+            Login,
+            Header,
+            TabBar
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
